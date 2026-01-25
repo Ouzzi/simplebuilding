ddad5004b06f4a16c2f4ab4b8bc90138ab567c6e
Oussama Azarfane <62378600+Ouzzi@users.noreply.github.com>
2026-01-25 14:12:05 +0100

    end update

--- src/main/java/com/simplebuilding/blocks/ModBlocks.java
+++ src/main/java/com/simplebuilding/blocks/ModBlocks.java
@@ -73,51 +73,28 @@ public class ModBlocks {
             ));
 
 
+
     public static final Block REINFORCED_HOPPER = registerBlock("reinforced_hopper", s -> new ModHopperBlock(s.strength(3.0F, 4.8F).nonOpaque().sounds(BlockSoundGroup.METAL), 1)); // 1 = Speed Multiplier (Basislogik wird im BE handled)
     public static final Block NETHERITE_HOPPER = registerBlock("netherite_hopper", s -> new ModHopperBlock(s.strength(5.0F, 1200.0F).nonOpaque().sounds(BlockSoundGroup.NETHERITE), 2));
-    // todo chest:
 
-    // public static final Block REINFORCED_CHEST = registerBlock("reinforced_chest",
-    //        s -> new ModChestBlock(s.strength(2.5F).nonOpaque(), ModChestBlock.Type.REINFORCED));
-    // public static final Block NETHERITE_CHEST = registerBlock("netherite_chest",
-    //        s -> new ModChestBlock(s.strength(5.0F, 1200.0F).nonOpaque(), ModChestBlock.Type.NETHERITE));
     public static final Block REINFORCED_PISTON = registerBlock("reinforced_piston", s -> new PistonBlock(false, s.strength(1.5F).sounds(BlockSoundGroup.METAL))); // sticky=false
     public static final Block NETHERITE_PISTON = registerBlock("netherite_piston", s -> new NetheriteBreakerPistonBlock(s.strength(5.0F, 1200.0F).sounds(BlockSoundGroup.NETHERITE)));
 
-
-    // --- FURNACES ---
     public static final Block REINFORCED_FURNACE = registerBlock("reinforced_furnace", s -> new ModFurnaceBlock(s.strength(3.5F).sounds(BlockSoundGroup.METAL), 1.25f)); // SpeedMultiplier wird hier nicht direkt genutzt, aber Konstruktor braucht ihn
     public static final Block NETHERITE_FURNACE = registerBlock("netherite_furnace", s -> new ModFurnaceBlock(s.strength(5.0F, 1200.0F).sounds(BlockSoundGroup.NETHERITE), 1.50f));
-    // --- SMOKERS ---
     public static final Block REINFORCED_SMOKER = registerBlock("reinforced_smoker", s -> new ModSmokerBlock(s.strength(3.5F).sounds(BlockSoundGroup.METAL), 1.25f));
     public static final Block NETHERITE_SMOKER = registerBlock("netherite_smoker", s -> new ModSmokerBlock(s.strength(5.0F, 1200.0F).sounds(BlockSoundGroup.NETHERITE), 1.50f));
-    // --- BLAST FURNACES ---
     public static final Block REINFORCED_BLAST_FURNACE = registerBlock("reinforced_blast_furnace", s -> new ModBlastFurnaceBlock(s.strength(3.5F).sounds(BlockSoundGroup.METAL), 1.25f));
     public static final Block NETHERITE_BLAST_FURNACE = registerBlock("netherite_blast_furnace", s -> new ModBlastFurnaceBlock(s.strength(5.0F, 1200.0F).sounds(BlockSoundGroup.NETHERITE), 1.50f));
 
 
     public static final Block NETHERITE_PISTON_HEAD = registerBlock("netherite_piston_head", s -> new NetheritePistonHeadBlock(s.noCollision().dropsNothing().sounds(BlockSoundGroup.NETHERITE)));
 
-    public static final Block ENDERITE_BLOCK = registerBlock("enderite_block",
-            unused -> new Block(AbstractBlock.Settings.create()
-                    .registryKey(keyOf("enderite_block")) // <--- DIESE ZEILE HAT GEFEHLT
-                    .mapColor(MapColor.BLACK)
-                    .requiresTool()
-                    .strength(50.0f, 1200.0f)
-                    .sounds(BlockSoundGroup.NETHERITE)));
+    public static final Block ENDERITE_BLOCK = registerBlock("enderite_block", unused -> new Block(AbstractBlock.Settings.create().registryKey(keyOf("enderite_block")).mapColor(MapColor.BLACK).requiresTool().strength(50.0f, 1200.0f).sounds(BlockSoundGroup.NETHERITE)));
 
-    public static final Block NIHILITH_ORE = registerBlock("nihilith_ore",
-            unused -> new Block(AbstractBlock.Settings.copy(Blocks.END_STONE)
-                    .registryKey(keyOf("nihilith_ore")) // <--- DIESE ZEILE HAT GEFEHLT
-                    .strength(25.0f, 1200.0f)
-                    .requiresTool()));
+    public static final Block NIHILITH_ORE = registerBlock("nihilith_ore", unused -> new Block(AbstractBlock.Settings.copy(Blocks.END_STONE).registryKey(keyOf("nihilith_ore")).strength(25.0f, 1200.0f).requiresTool()));
 
-    public static final Block ASTRALIT_ORE = registerBlock("astralit_ore",
-            unused -> new Block(AbstractBlock.Settings.copy(Blocks.END_STONE)
-                    .registryKey(keyOf("astralit_ore"))
-                    .strength(20.0f, 1200.0f)
-                    .luminance(state -> 5)
-                    .requiresTool()));
+    public static final Block ASTRALIT_ORE = registerBlock("astralit_ore", unused -> new Block(AbstractBlock.Settings.copy(Blocks.END_STONE).registryKey(keyOf("astralit_ore")).strength(20.0f, 1200.0f).luminance(state -> 5).requiresTool()));
 
     /**
      * Registriert einen Block und weist ihm vor der Erstellung den notwendigen RegistryKey zu.
